d84318a76356c9965761e8405a7dcfcfb658bd5b
joung1010 <79488695+joung1010@users.noreply.github.com>
2022-02-08 19:55:14 +0900

    JS home scroll fade

--- main.js
+++ main.js
@@ -27,6 +27,14 @@ contactBtn.addEventListener('click',(event) =>{
     scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", ()=>{
+    home.style.opacity =  1 - (window.scrollY/homeHeight);
+});
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
